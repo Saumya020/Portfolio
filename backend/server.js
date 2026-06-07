549a01765495d8061d18160d7bf75c4d06f47132
Saumya <saumyaverma0200@gmail.com>
2026-06-07 20:37:07 +0530

    changed env variable name

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.DATABASE_URL)
 .then(() => {
     console.log("MongoDB Connected");
 })
