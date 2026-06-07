9cab243dea3b844d424a5bdd21e2855c7ff54b92
Saumya <saumyaverma0200@gmail.com>
2026-06-07 13:03:25 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,71 @@
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+require("dotenv").config();
+
+const Contact = require("./models/Contact");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+
+.then(() => {
+    console.log("MongoDB Connected");
+})
+
+.catch((err) => {
+    console.log(err);
+});
+//route
+
+app.post("/contact", async (req, res) => {
+
+    try {
+
+        console.log(req.body);
+
+        const newContact = new Contact({
+
+            name: req.body.name,
+
+            email: req.body.email,
+
+            message: req.body.message
+
+        });
+
+        await newContact.save();
+
+        console.log("Data Saved");
+
+        res.status(200).json({
+            message: "Thank you for messaging!"
+        });
+
+    }
+
+    catch (error) {
+
+        console.log("ERROR:", error);
+
+        res.status(500).json({
+            message: "Error saving message"
+        });
+
+    }
+
+});
+
+app.listen(5000, () => {
+
+    console.log("Server running on port 5000");
+
+});
---
+++ fronetend/script.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("contactForm");
+
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const data = {
+
+        name: document.getElementById("name").value,
+
+        email: document.getElementById("email").value,
+
+        message: document.getElementById("message").value
+
+    };
+
+    try {
+
+        const response = await fetch("http://localhost:5000/contact", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(data)
+
+        });
+
+        const result = await response.json();
+
+        successMessage.innerText = result.message;
+
+        form.reset();
+
+    }
+
+    catch (error) {
+
+        console.log(error);
+
+        successMessage.innerText = "Something went wrong";
+
+    }
+
+});
